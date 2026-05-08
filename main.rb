82717ca66af26db59040eced467da4387f9b8992
Snakatakka <etwork563@gmail.com>
2026-05-08 19:16:54 -0400

    add introduction command

--- main.rb
+++ main.rb
@@ -3,4 +3,17 @@
 require 'discordrb'
 require 'dotenv'
 
-puts "Hello, World!"
+Dotenv.load("secrets.env")
+
+DISCORD_BOT_KEY = ENV['DISCORD_BOT_KEY']
+bot = Discordrb::Bot.new token: DISCORD_BOT_KEY
+
+bot.message(start_with: 's::introduction') do |event|
+    languages = ['hi!', 'hola!', 'bonjour!', 'oi!', '안녕!']
+    language_number = rand(1..5)
+    message = languages[language_number]
+    event.respond!(content: "#{message}, i\'m snakBot! a discord bot built by @snakatakka! i was programmed in ruby, and you can find the source code on snakatakka\'s github!")
+end
+
+# keeps the bot online
+bot.run
